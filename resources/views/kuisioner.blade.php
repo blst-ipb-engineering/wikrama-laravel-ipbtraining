13fee2947213a663e9882af0b28938b5bfb6c9f8
Rausan Fikri <59000362+rausanfikri@users.noreply.github.com>
2020-11-08 23:48:58 +0700

    Revisi 1.0

--- resources/views/kuisioner.blade.php
+++ resources/views/kuisioner.blade.php
@@ -6,13 +6,14 @@
 @endsection
 
 @section('main')
+<link rel="stylesheet" href="{{ mix('css/app.css') }}">
 <form method="POST">
   @csrf
   <div class="container-sm">
 
     <div class="row">
       <div class="col-sm-12 col-lg-6">
-        <img src="/images/pertanyaan/judul.png" alt="" class="img-fluid">
+        <img src="/images/pertanyaan/judul.png" alt="" class="img-fluid" width="75%">
       </div>
 
       <div class="col-sm-12 col-lg-6 mt-5">
@@ -29,7 +30,7 @@
     @foreach ($pertanyaan as $input)
     <div class="row mt-4">
       <div class="col-sm-12 col-lg-6">
-        <img src="{{ $input['imageUrl'] }}" alt="" class="img-fluid">
+        <img src="{{ $input['imageUrl'] }}" alt="" class="img-fluid" width="75%">
       </div>
 
       <div class="col-sm-12 col-lg-6 mt-5">
@@ -43,52 +44,6 @@
     <!-- I just wanna use react with firebase -->
     <!-- Not like this -->
     <!-- NOT LIKE THIS -->
-    <div class="row" id="fasilitas">
-      <div class="col-sm-12 mt-5 text-center">
-        <h1>Fasilitas Pendukung</h1>
-        <p>(Aplikasi penunjang, ruang pelatihan, makanan)</p>
-        <img v-if="!star1" src="/images/pertanyaan/star-empty.png" alt="" v-on:click="toggle1">
-        <img v-if="star1" src="/images/pertanyaan/star.png" alt="" v-on:click="toggle1">
-
-        <img v-if="!star2" src="/images/pertanyaan/star-empty.png" alt="" v-on:click="toggle2">
-        <img v-if="star2" src="/images/pertanyaan/star.png" alt="" v-on:click="toggle2">
-
-        <img v-if="!star3" src="/images/pertanyaan/star-empty.png" alt="" v-on:click="toggle3">
-        <img v-if="star3" src="/images/pertanyaan/star.png" alt="" v-on:click="toggle3">
-
-        <img v-if="!star4" src="/images/pertanyaan/star-empty.png" alt="" v-on:click="toggle4">
-        <img v-if="star4" src="/images/pertanyaan/star.png" alt="" v-on:click="toggle4">
-
-        <input type="text" name="fasilitas" v-bind:value="score" hidden>
-
-        <h3 class="mt-4">Komentar terkait fasilitas pendukung</h3>
-        <input type="text" class="form-control" name="fasilitasKomen" required>
-      </div>
-    </div>
-
-    <div class="row" id="attitude">
-      <div class="col-sm-12 mt-5 text-center">
-        <h1>Layanan Panitia</h1>
-        <p>(attitude)</p>
-        <img v-if="!star1" src="/images/pertanyaan/star-empty.png" alt="" v-on:click="toggle1">
-        <img v-if="star1" src="/images/pertanyaan/star.png" alt="" v-on:click="toggle1">
-
-        <img v-if="!star2" src="/images/pertanyaan/star-empty.png" alt="" v-on:click="toggle2">
-        <img v-if="star2" src="/images/pertanyaan/star.png" alt="" v-on:click="toggle2">
-
-        <img v-if="!star3" src="/images/pertanyaan/star-empty.png" alt="" v-on:click="toggle3">
-        <img v-if="star3" src="/images/pertanyaan/star.png" alt="" v-on:click="toggle3">
-
-        <img v-if="!star4" src="/images/pertanyaan/star-empty.png" alt="" v-on:click="toggle4">
-        <img v-if="star4" src="/images/pertanyaan/star.png" alt="" v-on:click="toggle4">
-
-        <input type="text" name="attitude" v-bind:value="score" hidden>
-
-        <h3 class="mt-4">Komentar terkait layanan panitia</h3>
-        <input type="text" class="form-control" name="attitudeKomen" required>
-      </div>
-    </div>
-
     <div class="row" id="kinerja">
       <div class="col-sm-12 mt-5 text-center">
         <h1>Layanan Panitia</h1>
@@ -108,16 +63,63 @@
         <input type="text" name="kinerja" v-bind:value="score" hidden>
 
         <h3 class="mt-4">Komentar terkait layanan panitia</h3>
-        <input type="text" class="form-control" name="kinerjaKomen" required>
+        <textarea type="text" class="form-control" name="kinerjaKomen" required></textarea>
+      </div>
+    </div>
+
+    <div class="row" id="fasilitas">
+      <div class="col-sm-12 mt-5 text-center">
+        <h1>Fasilitas Pendukung</h1>
+        <p>(Aplikasi penunjang, ruang pelatihan, makanan)</p>
+        <img v-if="!star1" src="/images/pertanyaan/star-empty.png" alt="" v-on:click="toggle1">
+        <img v-if="star1" src="/images/pertanyaan/star.png" alt="" v-on:click="toggle1">
+
+        <img v-if="!star2" src="/images/pertanyaan/star-empty.png" alt="" v-on:click="toggle2">
+        <img v-if="star2" src="/images/pertanyaan/star.png" alt="" v-on:click="toggle2">
+
+        <img v-if="!star3" src="/images/pertanyaan/star-empty.png" alt="" v-on:click="toggle3">
+        <img v-if="star3" src="/images/pertanyaan/star.png" alt="" v-on:click="toggle3">
+
+        <img v-if="!star4" src="/images/pertanyaan/star-empty.png" alt="" v-on:click="toggle4">
+        <img v-if="star4" src="/images/pertanyaan/star.png" alt="" v-on:click="toggle4">
+
+        <input type="text" name="fasilitas" v-bind:value="score" hidden>
+
+        <h3 class="mt-4">Komentar terkait fasilitas pendukung</h3>
+        <textarea type="text" class="form-control" name="fasilitasKomen" required></textarea>
+      </div>
+    </div>
+
+    <div class="row" id="attitude">
+      <div class="col-sm-12 mt-5 text-center">
+        <h1>Layanan Panitia</h1>
+        <p>(attitude)</p>
+        
+        <img v-if="!star1" src="/images/pertanyaan/star-empty.png" alt="" v-on:click="toggle1">
+        <img v-if="star1" src="/images/pertanyaan/star.png" alt="" v-on:click="toggle1">
+
+        <img v-if="!star2" src="/images/pertanyaan/star-empty.png" alt="" v-on:click="toggle2">
+        <img v-if="star2" src="/images/pertanyaan/star.png" alt="" v-on:click="toggle2">
+
+        <img v-if="!star3" src="/images/pertanyaan/star-empty.png" alt="" v-on:click="toggle3">
+        <img v-if="star3" src="/images/pertanyaan/star.png" alt="" v-on:click="toggle3">
+
+        <img v-if="!star4" src="/images/pertanyaan/star-empty.png" alt="" v-on:click="toggle4">
+        <img v-if="star4" src="/images/pertanyaan/star.png" alt="" v-on:click="toggle4">
+      
+        <input type="text" name="attitude" v-bind:value="score" hidden>
+
+        <h3 class="mt-4">Komentar terkait layanan panitia</h3>
+        <textarea type="text" class="form-control" name="attitudeKomen" required></textarea>
       </div>
     </div>
 
     <div class="row mt-4">
       <div class="col-sm-12 col-lg-6">
-        <img src="/images/pertanyaan/saran.png" alt="" class="img-fluid">
+        <img src="/images/pertanyaan/saran.png" alt="" class="img-fluid" width="75%">
       </div>
 
-      <div class="col-sm-12 col-lg-6 mt-5">
+      <div class="col-sm-12 col-lg-6 mt-5 w-75 p-3">
         <h1>Apakah Anda ingin mengembangkan training bersama kami? Topik apa yang ingin dikembangkan?</h1>
         <input type="text" name="saran" class="form-control">
       </div>
